989ef1334fbd82255e80fe8b28a723965e2c5562
Yoshihiro Tanaka <contact@cordea.jp>
2019-11-02 18:16:54 +0900

    add simple animation

--- app/src/main/java/jp/cordea/graphicscamerademo/FirstFragment.kt
+++ app/src/main/java/jp/cordea/graphicscamerademo/FirstFragment.kt
@@ -1,32 +1,46 @@
 package jp.cordea.graphicscamerademo
 
+import android.animation.ObjectAnimator
+import android.graphics.Camera
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_first.*
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class FirstFragment : Fragment() {
-
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
-    }
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_first, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment("From FirstFragment")
-            findNavController().navigate(action)
+            startAnimation()
         }
+    }
+
+    private fun startAnimation() {
+        val camera = Camera()
+        val matrix = background.imageMatrix
+        ObjectAnimator.ofFloat(0f, 50f)
+            .apply {
+                duration = 5000L
+                addUpdateListener {
+                    val value = it.animatedValue as Float
+                    camera.save()
+                    camera.translate(0f, 0f, value * 10)
+                    camera.rotateY(value)
+
+                    camera.getMatrix(matrix)
+                    background.imageMatrix = matrix
+                    camera.restore()
+                }
+            }
+            .start()
     }
 }
