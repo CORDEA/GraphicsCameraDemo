f1adf6bfd19ecfad9cbeecba483c3a0ac846094c
Yoshihiro Tanaka <contact@cordea.jp>
2019-11-03 09:48:03 +0900

    rename

--- app/src/main/java/jp/cordea/graphicscamerademo/SecondFragment.kt
+++ app/src/main/java/jp/cordea/graphicscamerademo/SecondFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_second.*
 
@@ -17,14 +16,15 @@ class SecondFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
+
+        animateButton.setOnClickListener {
             startAnimation()
         }
     }
 
     private fun startAnimation() {
-        image_view.startAnimation(
-            RotationAnimation(0f, 180f, image_view.width / 2f, image_view.height / 2f)
+        imageView.startAnimation(
+            RotationAnimation(0f, 180f, imageView.width / 2f, imageView.height / 2f)
                 .apply {
                     duration = 1000L
                 }
